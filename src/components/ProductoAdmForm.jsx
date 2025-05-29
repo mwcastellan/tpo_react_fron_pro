94cce984d8b6bc55d7b7b11eff017de2714f3d7a
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2025-05-29 00:55:04 -0300

    mwcastellan

--- src/components/ProductoAdmForm.jsx
+++ src/components/ProductoAdmForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 function ProductoAdmForm({ onAgregar }) {
   const [producto, setProducto] = useState({
     nombre: "",
@@ -12,6 +13,7 @@ function ProductoAdmForm({ onAgregar }) {
   });
 
   const [errores, setErrores] = useState({});
+  const navigate = useNavigate();
 
   const validarFormulario = () => {
     const nuevosErrores = {};
@@ -37,7 +39,7 @@ function ProductoAdmForm({ onAgregar }) {
     }
     if (!producto.categoria.trim() || producto.categoria.length < 10) {
       nuevosErrores.categoria =
-        "La descripción1 debe tener al menos 10 caracteres.";
+        "La categoría debe tener al menos 10 caracteres.";
     }
 
     setErrores(nuevosErrores);
@@ -52,10 +54,10 @@ function ProductoAdmForm({ onAgregar }) {
     onAgregar(producto); // Llamada a la función para agregar el producto
     setProducto({
       nombre: "",
-      precio: "0",
-      disponible: "0",
+      precio: 0,
+      disponible: 0,
       imagen: "",
-      cantidad: "0",
+      cantidad: 0,
       descripcion1: "",
       descripcion2: "",
       categoria: "",
@@ -68,107 +70,123 @@ function ProductoAdmForm({ onAgregar }) {
   };
 
   return (
-    <form className="ContactoFieldset card-body" onSubmit={handleSubmit}>
-      <div>
-        <label>Nombre:</label>
-        <input
-          type="text"
-          name="nombre"
-          value={producto.nombre}
-          onChange={handleChange}
-          required
-        />
-        {errores.nombre && <p style={{ color: "red" }}>{errores.nombre}</p>}
-      </div>
-      <div>
-        <label>Precio:</label>
-        <input
-          type="number"
-          name="precio"
-          value={producto.precio}
-          onChange={handleChange}
-          required
-          min="0"
-        />
-        {errores.precio && <p style={{ color: "red" }}>{errores.precio}</p>}
-      </div>
-      <div>
-        <label>Disponible:</label>
-        <input
-          type="number"
-          name="disponible"
-          value={producto.disponible}
-          onChange={handleChange}
-          required
-          min="0"
-        />
-        {errores.disponible && (
-          <p style={{ color: "red" }}>{errores.disponible}</p>
-        )}
-      </div>
-      <div>
-        <label>Imagen:</label>
-        <input
-          type="text"
-          name="imagen"
-          value={producto.imagen}
-          onChange={handleChange}
-          required
-        />
-        {errores.imagen && <p style={{ color: "red" }}>{errores.imagen}</p>}
-      </div>
-      <div>
-        <label>Cantidad:</label>
-        <input
-          type="number"
-          name="cantidad"
-          value={producto.cantidad}
-          onChange={handleChange}
-          required
-          min="0"
-        />
-        {errores.cantidad && <p style={{ color: "red" }}>{errores.cantidad}</p>}
-      </div>
-      <div>
-        <label>Descripcion1:</label>
-        <input
-          type="text"
-          name="descripcion1"
-          value={producto.descripcion1}
-          onChange={handleChange}
-          required
-        />
-        {errores.descripcion1 && (
-          <p style={{ color: "red" }}>{errores.descripcion1}</p>
-        )}
-      </div>
-      <div>
-        <label>Descripcion2:</label>
-        <input
-          type="text"
-          name="descripcion2"
-          value={producto.descripcion2}
-          onChange={handleChange}
-          required
-        />
-        {errores.descripcion2 && (
-          <p style={{ color: "red" }}>{errores.descripcion2}</p>
-        )}
-      </div>
-      <div>
-        <label>categoria:</label>
-        <input
-          type="text"
-          name="categoria"
-          value={producto.categoria}
-          onChange={handleChange}
-          required
-        />
-        {errores.categoria && (
-          <p style={{ color: "red" }}>{errores.categoria}</p>
-        )}
-      </div>
-      <button className="btn btn-secondary" type="submit">Agregar Producto</button>
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <article
+          className="card border-secondary"
+          style={{ maxWidth: "20rem" }}
+        >
+          <div className="card-body">
+            <label className="form-label">Nombre: </label>
+            <input
+              className="form-control"
+              type="text"
+              name="nombre"
+              value={producto.nombre}
+              onChange={handleChange}
+              required
+            />
+            {errores.nombre && <p style={{ color: "red" }}>{errores.nombre}</p>}
+
+            <label className="form-label">Precio:</label>
+            <input
+              className="form-control"
+              type="number"
+              name="precio"
+              value={producto.precio}
+              onChange={handleChange}
+              required
+              min="0"
+            />
+            {errores.precio && <p style={{ color: "red" }}>{errores.precio}</p>}
+
+            <label className="form-label">Disponible:</label>
+            <input
+              className="form-control"
+              type="number"
+              name="disponible"
+              value={producto.disponible}
+              onChange={handleChange}
+              required
+              min="0"
+            />
+            {errores.disponible && (
+              <p style={{ color: "red" }}>{errores.disponible}</p>
+            )}
+
+            <label className="form-label">Imagen:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="imagen"
+              value={producto.imagen}
+              onChange={handleChange}
+              required
+            />
+            {errores.imagen && <p style={{ color: "red" }}>{errores.imagen}</p>}
+
+            <label className="form-label">Cantidad:</label>
+            <input
+              className="form-control"
+              type="number"
+              name="cantidad"
+              value={producto.cantidad}
+              onChange={handleChange}
+              required
+              min="0"
+            />
+            {errores.cantidad && (
+              <p style={{ color: "red" }}>{errores.cantidad}</p>
+            )}
+
+            <label className="form-label">Descripción1:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="descripcion1"
+              value={producto.descripcion1}
+              onChange={handleChange}
+              required
+            />
+            {errores.descripcion1 && (
+              <p style={{ color: "red" }}>{errores.descripcion1}</p>
+            )}
+
+            <label className="form-label">Descripción2:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="descripcion2"
+              value={producto.descripcion2}
+              onChange={handleChange}
+              required
+            />
+            {errores.descripcion2 && (
+              <p style={{ color: "red" }}>{errores.descripcion2}</p>
+            )}
+
+            <label className="form-label">Categoría:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="categoria"
+              value={producto.categoria}
+              onChange={handleChange}
+              required
+            />
+            {errores.categoria && (
+              <p style={{ color: "red" }}>{errores.categoria}</p>
+            )}
+
+            <button className="btn btn-secondary" type="submit">
+              Agregar
+            </button>
+            <button className="btn btn-secondary" onClick={() => navigate(-1)}>
+              Cerrar
+            </button>
+          </div>
+        </article>
+      </fieldset>
     </form>
   );
 }
